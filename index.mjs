5257cb34a416beaa1a60640844c99e8ffa49d0ab
Shane Tindle <sha.tindle@gmail.com>
2025-08-18 09:53:08 -0500

    Added first pass at update tasks, though the matching still isn't quite right.  Still having to do delete/recreate and manual true-up

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import { getFilteredSyllabus } from "./canvas/syllabus.mjs";
 import { getProjects, getProjectTasks } from "./ticktick/projects.mjs";
-import { createTask } from "./ticktick/tasks.mjs";
+import { createTask, updateTask } from "./ticktick/tasks.mjs";
 import canvas_courses from "./canvas_courses.json" with { type: "json" };
 import 'dotenv/config'
 import { getCourses } from "./canvas/courses.mjs";
@@ -8,7 +8,7 @@ import { getCourses } from "./canvas/courses.mjs";
 const projects = await getProjects(process.env.ticktick_access_token);
 const projectId = projects.find(t => t.name === canvas_courses.ticktick_project_name).id;
 
-const createAllTasks = async (canvas_course, projectId) => {
+const createAllTasks = async (canvas_course, projectId, existingTasks) => {
     const syllabus = await getFilteredSyllabus(process.env.canvas_url, process.env.canvas_session, canvas_course);
 
     const END_DATE = Date.parse("2025-08-31T00:00:00").valueOf();
@@ -20,15 +20,28 @@ const createAllTasks = async (canvas_course, projectId) => {
     for (const item of syllabus) {
         let start_at = new Date(item.start_at);
 
+        if (item.context_name === "Clubs") continue;
+
         if (start_at.valueOf() < END_DATE && start_at.valueOf() > YESTERDAY) {
-            /* !tasks.find(t => t.title === taskName) && */
-            await createTask(process.env.ticktick_access_token, 
-                item.title,
-                projectId,
-                item.all_day,
-                item.start_at?.replace("Z","+0000"),
-                item.end_at?.replace("Z","+0000"),
-                [item.context_name, canvas_courses.student]);
+            const existingTask = existingTasks.find(t => t.title?.toLowerCase() === item.title?.toLowerCase() && (item.context_name === null ?? t.tags.find(x => x.toLowerCase() === item.context_name.toLowerCase())));
+            if (existingTask) {
+                await updateTask(process.env.ticktick_access_token, 
+                    existingTask.id,
+                    item.title,
+                    projectId,
+                    item.all_day,
+                    item.start_at?.replace("Z","+0000"),
+                    item.end_at?.replace("Z","+0000"),
+                    [item.context_name, canvas_courses.student]);
+            } else {
+                await createTask(process.env.ticktick_access_token, 
+                    item.title,
+                    projectId,
+                    item.all_day,
+                    item.start_at?.replace("Z","+0000"),
+                    item.end_at?.replace("Z","+0000"),
+                    [item.context_name, canvas_courses.student]);
+            }
 
             console.log(item.title);
 
@@ -40,8 +53,10 @@ const createAllTasks = async (canvas_course, projectId) => {
 const courses = await getCourses(process.env.canvas_url, process.env.canvas_session);
 const courseIds = [...courses.map(t => t.id)];
 
+const existingTasks = await getProjectTasks(process.env.ticktick_access_token, projectId);
+
 for (const course_id of courseIds) 
-    await createAllTasks(`course_${course_id}`, projectId)
+    await createAllTasks(`course_${course_id}`, projectId, existingTasks)
 
 
 console.log("exit");
--- ticktick/tasks.mjs
+++ ticktick/tasks.mjs
@@ -30,6 +30,40 @@ export const createTask = async (
     await fetch("https://ticktick.com/open/v1/task", requestOptions);
 }
 
+export const updateTask = async (
+    ticktick_access_token,
+    id,
+    title,
+    projectId,
+    isAllDay,
+    startDate,
+    dueDate,
+    tags
+) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", `Bearer ${ticktick_access_token}`);
+
+    const raw = JSON.stringify({
+        id,
+        title,
+        projectId,
+        isAllDay,
+        "startDate": startDate,
+        "dueDate": dueDate,
+        tags
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    await fetch(`https://ticktick.com/open/v1/task/${id}`, requestOptions);
+}
+
 export const deleteTask = async (ticktick_access_token, projectId, taskId) => {
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
